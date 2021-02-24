2d36b49cb7730fff0f73ef96d255102431495d74
Emil Dafinov <EmilDafinov@users.noreply.github.com>
2021-02-23 21:52:20 -0500

    Imports fixed so WordSpec is available

--- src/main/g8/src/test/scala/$organization__packaged$/$name;format="word,lower"$/util/UnitTestSpec.scala
+++ src/main/g8/src/test/scala/$organization__packaged$/$name;format="word,lower"$/util/UnitTestSpec.scala
@@ -1,12 +1,14 @@
 package $organization$.$name;format="word,lower"$.util
 
 import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.matchers.should.Matchers
 import org.scalatest.time.SpanSugar
-import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, Matchers, WordSpec}
+import org.scalatest.wordspec.AnyWordSpecLike
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
 import org.scalatestplus.mockito.MockitoSugar
 
 trait UnitTestSpec
-  extends WordSpec
+  extends AnyWordSpecLike
     with Matchers
     with ScalaFutures
     with MockitoSugar
